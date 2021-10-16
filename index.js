6ae0195ede9ce331020bb82b52aea961b266084a
Juni Terry <juniterry@Junis-MacBook-Pro.local>
2021-10-16 17:12:17 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+let port = process.env.PORT || 3000;
+const importData = require("./data.json")
+
+app.get("/", (req, res) => {
+    res.send("hello world")
+})
+
+app.get("/numbers", (req, res) => {
+    res.send(importData)
+})
+
+app.listen(port, () => {
+    console.log(`Example app is listening on http//:localhost:${port}`)
+})
