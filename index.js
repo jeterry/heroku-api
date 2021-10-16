654e8eda292510e5fc02f5293c4b566c1505a2d9
Juni Terry <juniterry@Junis-MacBook-Pro.local>
2021-10-16 17:31:09 -0500

    working on post

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+
 const app = express();
+app.use(express.urlencoded({extended:true}));
 let port = process.env.PORT || 3000;
 const importData = require("./data.json")
+
+app.post('/post-test', (req, res) => {
+    console.log('Got body:', req.body);
+    res.sendStatus(200);
+});
 
 app.get("/", (req, res) => {
     res.send("hello world")
