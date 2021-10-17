66c06b9063db0a213cc970b3bdecce17c3fd5e53
Juni Terry <juniterry@Junis-MacBook-Pro.local>
2021-10-17 10:38:24 -0500

    experimenting to get a database connected

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
 const express = require("express");
+
 const app = express();
 app.use(express.urlencoded({extended:true}));
+let port = process.env.PORT || 3000;
 
-const {Pool} = require('pg');
-const pool = new Pool ({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-});
+app.get("/", (req, res) => {
+    res.send("hello world")
+})
 
-app.get('/', function(req, res){
-    res.render('index', {title: 'Express'});
-});
-
-app.get('/db', async(req, res) => {
-    try {
-        const client = await pool.connect()
-        const result = await client.query('SELECT * FROM numtracker');
-        const results = {'results': (result) ? result.rows : null}
-        res.send(JSON.stringify(results))
-        client.release();
-    }
-    catch (err){
-        console.error(err);
-        res.send('Error' + err)
-    }
+app.listen(port, () => {
+    console.log(`Example app is listening on http//:localhost:${port}`)
 })
