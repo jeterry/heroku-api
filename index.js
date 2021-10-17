20c4faa75b281a40885b3df49580e3b88b0efc4a
Juni Terry <juniterry@Junis-MacBook-Pro.local>
2021-10-17 10:39:33 -0500

    experimenting to get a database connected

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app = express();
 app.use(express.urlencoded({extended:true}));
 let port = process.env.PORT || 3000;
 
+const {Pool} = require('pg');
+const pool = new Pool ({
+   connectionString: process.env.DATABASE_URL,
+   ssl: true
+});
+
+
 app.get("/", (req, res) => {
     res.send("hello world")
 })
