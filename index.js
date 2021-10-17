a6414eaca36135c6ba9adc443f36013d1e663512
Juni Terry <juniterry@Junis-MacBook-Pro.local>
2021-10-17 10:28:47 -0500

    experimenting to get a database connected

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
+const { Router, query } = require("express");
 const express = require("express");
-
 const app = express();
 app.use(express.urlencoded({extended:true}));
-let port = process.env.PORT || 3000;
-const db = require('./queries')
 
-app.get("/", (req, res) => {
-    res.send("hello world")
-})
+const {Pool} = require('pg');
+const pool = new Pool ({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+});
 
-app.get('/sum', db.getSum)
-app.post('/sum', db.updateSum)
-app.put('/sum', db.getSum)
+app.get('/', function(req, res, next){
+    res.render('index', {title: 'Express'});
+});
 
-app.listen(port, () => {
-    console.log(`Example app is listening on http//:localhost:${port}`)
+app.get('/db', async(req, res) => {
+    try {
+        const client = await pool.connect()
+        const result = await client.query('SELECT * FROM numtracker');
+        const results = {'results': (result) ? result.rows : null}
+        res.send(JSON.stringify(results))
+        client.release();
+    }
+    catch (err){
+        console.error(err);
+        res.send('Error' + err)
+    }
 })
