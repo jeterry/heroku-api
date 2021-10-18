8314a79cff4a0a7af59b2dd96b9d3f98cca01381
Juni Terry <juniterry@Junis-MacBook-Pro.local>
2021-10-17 20:22:41 -0500

    adding back in queries file

--- index.js
+++ index.js
@@ -5,13 +5,8 @@ app.use(express.urlencoded({extended:true}));
 let port = process.env.PORT || 3000;
 const db = require('./queries')
 
-app.get("/", (req, res) => {
-    res.send('hello')
-})
-
-app.get('/sum', db.getSum)
-app.post('/sum', db.updateSum)
-app.put('/sum', db.getSum)
+app.get('/', db.getSum)
+app.post('/', db.updateSum)
  
 
 app.listen(port, () => {
