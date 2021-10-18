b6b0f9dfdd2a1b39a2ef370e28daf7440e889fb7
Juni Terry <juniterry@Junis-MacBook-Pro.local>
2021-10-17 21:33:49 -0500

    messing around with a few things.

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ app.use(express.urlencoded({extended:true}));
 let port = process.env.PORT || 3000;
 const db = require('./queries')
 
-app.get('/', db.getSum)
-app.post('/', db.updateSum)
+app.get('/sum', db.getSum)
+app.post('/sum', db.updateSum)
  
 
 app.listen(port, () => {
